d3fabbe6a35a4c11d3d29720c5155ed0f4fdb58d
Chris Eldridge <cre@mojotech.com>
2016-10-31 15:32:26 -0400

    request bus positions from ripta

--- app/controllers/ripta_controller.rb
+++ app/controllers/ripta_controller.rb
@@ -1,5 +1,7 @@
 class RiptaController < ApplicationController
   def index
-    render json: {ripta: 'ripta'}, status: 201
+    response = HTTParty.get('http://realtime.ripta.com:81/api/vehiclepositions?format=json')
+
+    render json: {ripta: response}, status: 201
   end
 end
